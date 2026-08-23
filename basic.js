d3a897c2fbae198ae6e1e67903c751e82edebc46
SyedaRida707 <syedarida953@gmail.com>
2026-08-23 11:54:46 +0500

    start 23-august-2026

---
+++ basic.js
@@ -0,0 +1,14 @@
+// ========================================
+// What is javascript?
+// javascript improves the user experience of the web page by converting it from a
+// static page into an interactive one.
+
+// OR
+
+// javascript is used to update and change both HTML and CSS.
+// it adds behaviour to web pages.
+// $0 on console change by $0
+
+// =========================================
+// History of javascript
+// in 1995 created by brendan Eich at netscape in just 10 days.
