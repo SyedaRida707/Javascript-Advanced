9352f2b19b015ed78e8e15d55edb58340b9dc3cf
SyedaRida707 <syedarida953@gmail.com>
2026-08-23 12:28:10 +0500

    start 23-august-2026

--- basic.js
+++ basic.js
@@ -12,3 +12,35 @@
 // =========================================
 // History of javascript
 // in 1995 created by brendan Eich at netscape in just 10 days.
+
+
+// It shows a popup box in the browser.
+// 'hi js' is the message displayed inside the popup.
+// alert('hi js');
+
+// console.log() is used to print/display something in the browser console.
+// 'hi i am print on console' is the message you want to print.
+// You can see it in Browser → Inspect → Console.
+// console.log('hi i am print on console');
+
+// three ways to use js
+
+
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental 
+// concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with.
+//  It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name 
+// and can be used to store and manipulate data in a program.
+
+var my_data = 'rida'
+var $myage = 21;
+var myEmail = 'rida@gmail.com'
+
+
