92c5a33bb9ca6abd211bea130172ae197bb2f825
SyedaRida707 <syedarida953@gmail.com>
2026-08-25 16:06:23 +0500

    start 23-august-2026

--- 3-loops.js
+++ 3-loops.js
@@ -422,9 +422,9 @@
 
 
 // for (var i = 1; i <= 5; i++) {
-//   var pattern = "";
-//   for (var j = 1; j <= i; j++) {
-//     pattern = pattern + " *";
-// }
-// console.log(pattern);
+//     var pattern = '';
+//     for (var j = 1; j <= i; j++) {
+//         pattern = pattern + " *";
+//     }
+//     console.log(pattern);
 // }
