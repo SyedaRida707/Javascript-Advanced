e9c73bc00ab20327cea31d3e77da386c32c5d2cc
SyedaRida707 <syedarida953@gmail.com>
2026-09-13 14:26:01 +0500

    ok

---
+++ 8-localstorage.js
@@ -0,0 +1,55 @@
+//============================== LocalStorage ====================================
+// The localStorage object allows you to save key/value pairs in the browser.
+// The localStorage object stores data with no expiration date.
+// The data is not deleted when the browser is closed and is available for future sessions.
+// What we will cover
+// ✱ How to add / store data in localStorage.
+// ✱ How to get / retrieve data from localStorage.
+// ✱ How to remove data from localStorage.
+
+
+
+// How to add the data from localStorage
+// localStorage.setItem("user", "rida");
+
+// localStorage.clear();
+
+// How to get the data from localStorage
+// localStorage.getItem("user");
+// console.log(localStorage.getItem('user'));
+
+// How to remove the data from localStorage
+// localStorage.removeItem("user");
+
+// Before storing an object/array in LocalStorage → JSON.stringify()
+// After getting an object/array from LocalStorage → JSON.parse()
+
+// Method	Converts
+// JSON.stringify()	JavaScript Object → JSON String
+// JSON.parse()	JSON String → JavaScript Object
+
+// STRINGIFY → Make it a string
+// PARSE → Convert the string back
+
+// todo Local Storage can only store strings, so when you want to store a complex data structure 
+// like an array or an object, you need to convert it to a string using JSON.stringify:
+
+const data = { name: "rida", age: 21, city: "karachi" };
+// const jsonString = JSON.stringify(data)
+// console.log(jsonString);
+// Output: '{"name":"Vinod","age":30,"city":"pune"}'
+
+const jsonString = localStorage.setItem('userinfo', JSON.stringify(data));
+// console.log(JSON.parse(localStorage.getItem('userinfo')));
+
+// JSON.parse: Converts a JSON string into a JavaScript object.
+// Useful when you receive JSON data from a server or read it from a file,
+// and you want to work with it as a JavaScript object.
+
+// const jsonString = '{"name":"rida","age":21,"city":"karahi"}';
+// const parsedData = JSON.parse(jsonString);
+// console.log(parsedData);
+// Output: { name: 'Vinod', age: 30, city: 'pune' }
+
+const getobject = JSON.parse(localStorage.getItem('userinfo'));
+console.log(getobject);
