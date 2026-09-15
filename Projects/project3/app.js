558d2af08ded9599399a37d83ca896acc63cac6f
SyedaRida707 <syedarida953@gmail.com>
2026-09-15 17:36:31 +0500

    ok

--- Projects/project3/app.js
+++ Projects/project3/app.js
@@ -1,3 +1,17 @@
 let addBtn = document.querySelector('#addbtn');
 let input = document.querySelector('#inputfield');
 let divShow = document.querySelector('#interact');
+
+const addTodoList = (event) => {
+    event.preventDefault();
+    console.log('test');
+    const div = document.createElement('div');
+    div.classList.add('main2');
+    div.innerHTML = `<p>${input.value}</p>
+            <button id='deletebtn'>Delete</button>`;
+    divShow.append(div);
+}
+
+addBtn.addEventListener('click', (e) => {
+    addTodoList(e);
+});
