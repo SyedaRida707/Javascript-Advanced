dc5f89a16aa73b091217d2da0d7b049ce27747ee
SyedaRida707 <syedarida953@gmail.com>
2026-09-15 18:13:35 +0500

    ok

--- Projects/project3/app.js
+++ Projects/project3/app.js
@@ -1,17 +1,34 @@
 let addBtn = document.querySelector('#addbtn');
 let input = document.querySelector('#inputfield');
 let divShow = document.querySelector('#interact');
+let addValueArray = [];
+
+
+const getTodoListFromLocal = () =>{
+  return  JSON.parse(localStorage.getItem('todo'));
+}
 
 const addTodoList = (event) => {
     event.preventDefault();
-    console.log('test');
+
+    addValueArray = getTodoListFromLocal() || [];
+    const todoValue = input.value.trim();
+
+    addValueArray.push(todoValue);
+    addValueArray = [...new Set(addValueArray)];
+    console.log(addValueArray);
+    localStorage.setItem('todo',JSON.stringify(addValueArray));
+
+
     const div = document.createElement('div');
     div.classList.add('main2');
     div.innerHTML = `<p>${input.value}</p>
             <button id='deletebtn'>Delete</button>`;
     divShow.append(div);
+
 }
 
 addBtn.addEventListener('click', (e) => {
     addTodoList(e);
 });
+
