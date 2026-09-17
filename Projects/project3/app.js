5d000d8028e09c9f93381b24cf862f38ccb50608
SyedaRida707 <syedarida953@gmail.com>
2026-09-17 18:40:26 +0500

    ok

--- Projects/project3/app.js
+++ Projects/project3/app.js
@@ -6,8 +6,8 @@ let divShow = document.querySelector('#interact');
 const getTodoListFromLocal = () => {
     return JSON.parse(localStorage.getItem('todo'));
 }
-const addagain = (addValueArray) =>{
- localStorage.setItem('todo',JSON.stringify(addValueArray));
+const addagain = (addValueArray) => {
+    localStorage.setItem('todo', JSON.stringify(addValueArray));
 }
 
 const showDynamic = (e) => {
@@ -25,19 +25,19 @@ const addTodoList = (event) => {
 
     const todoValue = input.value.trim();
     input.value = '';
-    
-    if(todoValue != '' && !addValueArray.includes(todoValue)){
-        addValueArray.push(todoValue);
-    addValueArray = [...new Set(addValueArray)];
-    console.log(addValueArray);
-    localStorage.setItem('todo', JSON.stringify(addValueArray));
 
-    showDynamic(todoValue);
+    if (todoValue != '' && !addValueArray.includes(todoValue)) {
+        addValueArray.push(todoValue);
+        addValueArray = [...new Set(addValueArray)];
+        // console.log(addValueArray);
+        localStorage.setItem('todo', JSON.stringify(addValueArray));
+
+        showDynamic(todoValue);
     }
 }
 
 const showData = () => {
-    console.log(addValueArray);
+    // console.log(addValueArray);
     addValueArray.forEach((element) => {
         showDynamic(element);
     });
@@ -47,28 +47,32 @@ const showData = () => {
 showData();
 
 // remove
-const deletetodo = (e) =>{
-    console.log(e.target);
+const deletetodo = (e) => {
     let getodo = e.target;
     let removetodo = getodo.previousElementSibling.innerText;
-    console.log(removetodo);
-    
-    addValueArray = addValueArray.filter((e)=>{
-     console.log(e);
-     return e != removetodo.toLowerCase();
+    let parentElem = getodo.parentElement;
+    // console.log(removetodo);
+
+    addValueArray = addValueArray.filter((e) => {
+        console.log(e);
+        return e != removetodo.toLowerCase();
     });
 
     addagain(addValueArray);
-
-    console.log(addValueArray);
-    
+    parentElem.remove();
+    // console.log(addValueArray);   
 }
+
+divShow.addEventListener('click', (e) => {
+    console.log(e.target);
+
+    if (e.target.classList.contains('deletebtn')) {
+        deletetodo(e);
+    }
+});
 
 addBtn.addEventListener('click', (e) => {
     addTodoList(e);
 });
 
 
-divShow.addEventListener('click',(e)=>{
-    deletetodo(e);
-});
