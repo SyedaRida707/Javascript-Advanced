0ca8bc84210568aedd98efa8c36f52b31fdfcf61
SyedaRida707 <syedarida953@gmail.com>
2026-09-20 21:37:24 +0500

    ok

---
+++ how_js_works/3-call-stack.js
@@ -0,0 +1,18 @@
+//  function firstFunction() {
+//         console.log("First function");
+//         secondFunction();
+//       }
+
+//       function secondFunction() {
+//         console.log("Second function");
+//         setTimeout(() => {
+//           console.log(`fun2 is starting`);
+//         }, 3000);
+//         thirdFunction();
+//       }
+
+//       function thirdFunction() {
+//         console.log("Third function");
+//       }
+
+//       firstFunction();
